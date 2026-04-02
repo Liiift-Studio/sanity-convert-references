2c448cdfdc93661c932144dcda367149cde13aae
Quinn Keaveney <qkeave@gmail.com>
2026-04-02 01:22:37 -0700

    Remove node_modules and dist from git tracking, add .gitignore

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Sanity Convert References - Main Export
- * Convert between strong and weak references in Sanity documents
- */
-export { default as ConvertReferences } from './ConvertReferences';
-export { default } from './ConvertReferences';
-export type { ConvertReferencesProps, ConversionResult } from './ConvertReferences';
